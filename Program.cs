36d7310a237e1922268c9fa0577434b16f707c7c
pratham000112 <virmanipratham2@gmail.com>
2026-04-27 10:18:22 +0530

    second

--- Program.cs
+++ Program.cs
@@ -9,3 +9,22 @@ double price = 99.99;
 int product = (int)price;
 
 Console.WriteLine(product);
+
+// conversing using convert classes
+
+string number = "123";
+
+int result = Convert.ToInt32(number);
+
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+    
+
+
