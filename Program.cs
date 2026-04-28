a4c398e6a3a14ec2f9c3ade87069c2ea5fb4ff1a
pratham000112 <virmanipratham2@gmail.com>
2026-04-28 08:54:36 +0530

    method overloading

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
 ﻿
 // explicit  casting
 
-using System;
+//using System;
 
 
-double price = 99.99;
+//double price = 99.99;
 
-int product = (int)price;
+//int product = (int)price;
 
-Console.WriteLine(product);
+//Console.WriteLine(product);
 
-// conversing using convert classes
+//// conversing using convert classes
 
-string number = "123";
+//string number = "123";
 
-int result = Convert.ToInt32(number);
+//int result = Convert.ToInt32(number);
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
 
 
 
@@ -24,15 +24,43 @@ Console.WriteLine(result);
 // Default Parameter.
 
 
-static void MyMethod(string country = "London")
-{
-    Console.WriteLine(country +  "this is the  Most visited Country");
-}
+//static void MyMethod(string country = "London")
+//{
+//    Console.WriteLine(country  +  " this is the  Most visited Country ");
+//}
 
-    MyMethod("India");
-    MyMethod("USA");
-    MyMethod();
-    MyMethod("Japan");
+//    MyMethod("India");
+//    MyMethod("USA");
+//    MyMethod();
+//    MyMethod("Japan");
+
+// Method Overloading
+
+ class Program
+{
+    static int add (int a, int b)
+    {
+        return a + b;
+    }
+    static int add (int a, int b, int c)
+        {
+        return a + b + c;
+    }
+
+    static double add  (double a , double b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(add(10, 20));
+       Console.WriteLine(add(10, 20, 30));
+        Console.WriteLine(add(10.5, 20.5));
+    }
+
+
+}
 
 
 
