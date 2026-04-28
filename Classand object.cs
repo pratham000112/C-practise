cd89ae7b68ae2ccb18178d95306177f9f9ccebe0
pratham000112 <virmanipratham2@gmail.com>
2026-04-28 23:16:32 +0530

    class and object

---
+++ Classand object.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+
+//class Car
+//{
+//    public String color = "Black";
+
+//    public void drive()
+//    {
+//        Console.WriteLine("Car is driving");
+//    }
+
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Car mycar = new Car();
+//            Console.WriteLine(mycar.color);
+//            mycar.drive();
+
+//        }
+//    }
+//}
+
+
+
+class  Student
+{
+    public string Name;
+    public int  Age;
+
+
+    public void Display()
+    {
+        Console.WriteLine("Name : " + Name + ", Age " + Age);
+
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s1 = new Student();
+        s1.Name = "Harshita Sachdeva";
+        s1.Age = 20;
+
+        Student s2 = new Student();
+        s2.Name = "Pulkit virmani";
+        s2.Age = 21;
+
+
+        Student s3 = new Student();
+        s3.Name = "kritika Sethi";
+              s3.Age = 28;
+
+        s1.Display();
+        s2.Display();
+        s3.Display();
+    }
+
+
+      
+}
+
+
--- Program.cs
+++ Program.cs
@@ -36,31 +36,31 @@
 
 // Method Overloading
 
- class Program
-{
-    static int add (int a, int b)
-    {
-        return a + b;
-    }
-    static int add (int a, int b, int c)
-        {
-        return a + b + c;
-    }
+// class Program
+//{
+//    static int add (int a, int b)
+//    {
+//        return a + b;
+//    }
+//    static int add (int a, int b, int c)
+//        {
+//        return a + b + c;
+//    }
 
-    static double add  (double a , double b)
-    {
-        return a + b;
-    }
+//    static double add  (double a , double b)
+//    {
+//        return a + b;
+//    }
 
-    static void Main(string[] args)
-    {
-        Console.WriteLine(add(10, 20));
-       Console.WriteLine(add(10, 20, 30));
-        Console.WriteLine(add(10.5, 20.5));
-    }
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine(add(10, 20));
+//       Console.WriteLine(add(10, 20, 30));
+//        Console.WriteLine(add(10.5, 20.5));
+//    }
 
 
-}
+//}
 
 
 
