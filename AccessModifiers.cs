6fcae523aa2c2120fcc3e9559d4859abef6b75dd
pratham000112 <virmanipratham2@gmail.com>
2026-05-08 10:22:26 +0530

    first commit

---
+++ AccessModifiers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Newstudent
+{
+    public string name = "Harshita Sachdeva";
+
+    public void showname()
+    {
+        Console.WriteLine(name);
+    }
+
+
+}
+
+  class Program
+ {
+    static void Main()
+    {
+        Newstudent s = new Newstudent();
+
+        Console.WriteLine(s.name);
+
+        s.showname();
+    }
+
+}
--- Classand object.cs
+++ Classand object.cs
@@ -28,41 +28,41 @@ using System.Security.Cryptography;
 
 
 
-class  Student
-{
-    public string Name;
-    public int  Age;
+//class  Student
+//{
+//    public string Name;
+//    public int  Age;
 
 
-    public void Display()
-    {
-        Console.WriteLine("Name : " + Name + ", Age " + Age);
+//    public void Display()
+//    {
+//        Console.WriteLine("Name : " + Name + ", Age " + Age);
 
-    }
+//    }
 
-}
+//}
 
-class Program
-{
-    static void Main()
-    {
-        Student s1 = new Student();
-        s1.Name = "Harshita Sachdeva";
-        s1.Age = 20;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Student s1 = new Student();
+//        s1.Name = "Harshita Sachdeva";
+//        s1.Age = 20;
 
-        Student s2 = new Student();
-        s2.Name = "Pulkit virmani";
-        s2.Age = 21;
+//        Student s2 = new Student();
+//        s2.Name = "Pulkit virmani";
+//        s2.Age = 21;
 
 
-        Student s3 = new Student();
-        s3.Name = "kritika Sethi";
-        s3.Age = 28;
+//        Student s3 = new Student();
+//        s3.Name = "kritika Sethi";
+//        s3.Age = 28;
 
-        s1.Display();
-        s2.Display();
-        s3.Display();
-    }
+//        s1.Display();
+//        s2.Display();
+//        s3.Display();
+    
 
     // public car (string b)
     //{
@@ -77,7 +77,7 @@ class Program
     // {
     //name = n;
     // salary = 0;
-}
+
 
   
     // public Employee (string n, int s)
